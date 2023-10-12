7fb00c81b2da19d648da6f2f6b32e8d026cedabf
bertrandkuniedal <bertrandkuniedal@educ.college-montreal.qc.ca>
2023-10-12 15:56:30 -0400

    ajojuter que le utilisatuer peu changer qqchose

--- main.py
+++ main.py
@@ -1,44 +1,53 @@
-##Faire randomint et donner des "plus haut" ou "plus bas" a l'utilisateur
+# Faire randomint et donner des "plus haut" ou "plus bas" a l'utilisateur
 # importer random pour un random number generator
 import random
 
-a = 0
-b = 100
-#faire une loop pour que le jeu puisse continer meme apres qu'il finisse
+
+# faire une loop pour que le jeu puisse continer meme apres qu'il finisse
 jeux = True
 
-def choix(a,b):
+# Fonction qui retourne le premier et le dernier nombre de la borne
+
+
+def choix():
+    global a, b
     a = int(input("Choissisez le premier nombre: "))
     b = int(input("Choissisez le deuxieme nombre: "))
     return a, b
 
 
-
-
-
 # Loop du jeu
 while jeux:
+    global a, b, nb
     print("Voulez vous garder les bornes (0,100) ou les changer?")
     change = input("G pour les garder C pour les changer: ")
     change = change.strip()
-    if change == "g" or "G":
+    if change == "g":
         print("Elles ne vont pas changer: (0,100)")
-        nb = random.randint(0,100)
+        nb = random.randint(0, 100)
         print("J'ai choisi un nombre au hasard entre 0 et 100, a vous de deviner lequel")
-    elif change == "c" or "C":
-        choix(a, b)
-        nb = random.randint(a, b)
+    elif change == "c":
+        choix()
+        # Si l'utilisateur fait une erreur et inverse les nombres
+        if a > b:
+            print("Vous avez inversé les bornes! Je pense donc que vous vouliez dire que nombre sera entre ", b, " et ",
+                  a)
+            nb = random.randint(b, a)
+        # Si tout va bien
+        else:
+            print("Votre nombre sera entre ", a, " et ", b)
+            nb = random.randint(a, b)
     essai = 0
     tries = 1
 
-    #Loop de essai jusqua bon nombre
+    # Loop de essai jusqua bon nombre
     while essai != nb:
-        essai= int(input("Votre essai:"))
-        #Si le nombre randomly generated est plus grand
+        essai = int(input("Votre essai:"))
+        # Si le nombre randomly generated est plus grand
         if essai > nb:
             print("Le nombre est plus petit! Votre essai: ", essai)
             tries += 1
-        #Si le nombre randomly gnerated est plus petit
+        # Si le nombre randomly gnerated est plus petit
         if essai < nb:
             print("Le nombre est plus grand! Votre essai: ", essai)
             tries += 1
@@ -50,5 +59,5 @@ while jeux:
         print("Recommencons!")
     if restart == "n":
         print("Merci et au revoir!")
-        #rendre la variable jeux a false pour que le jeux ne recommence pas
+        # Rendre la variable jeux a false pour que le jeux ne recommence pas
         jeux = False
